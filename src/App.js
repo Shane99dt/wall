d5390f7479c390759913fd42ca34a90262dde1b7
Shane99dt <dushen1999thilaksha@gmail.com>
2022-07-12 10:47:48 +0200

    wall exercise

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Card from './components/Card'
 
 class App extends React.Component {
   render() {
+    // const array = JSON.parse(users)
     return (
-      // hello
+      <div className="container-fluid d-flex flex-column justify-content-start">
+        <h1 className="mt-3 mx-5">The wall</h1>
+        <div className="d-flex flex-row flex-wrap justify-content-center">
+          {users.map(user => {
+            return (
+              <Card name = {user.name} picture={user.picture} about={user.about} />
+              )
+            })}
+        </div>
+      </div>
     )
   }
 }
 
-export default App 
+export default App
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,19 @@ import React from 'react'
 class Card extends React.Component {
   render() {
     return (
-      // hello
+      <div className="d-flex flex-column border border col-5 m-2">
+        <article className="d-flex flex-row px-4 pt-4">
+          <img src= {this.props.picture} height="120px" width="120px" ></img>
+          <h4 className="align-self-center p-4">{this.props.name}</h4>
+        </article>
+        <article className='p-4'>
+          <p>
+            {this.props.about}
+          </p>
+        </article>
+    </div>
     )
   }
 }
 
-export default Card 
+export default Card
